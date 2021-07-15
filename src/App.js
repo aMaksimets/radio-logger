2f4d4789e9d83f33d29e9a6308a13332c1d293ad
Greg Bowne <gbowne144@gmal.com>
2021-07-14 17:28:04 -0700

    updated.  Added contest log  some path routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MyNav from "./components/MyNav.js";
-import { BrowserRouter, Switch, Route, Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import HamLog from "./components/HamLog";
 import SwlLog from "./components/SwlLog";
@@ -8,21 +8,32 @@ import SwlLog from "./components/SwlLog";
 function App() {
   
   return (
+    <Router>
     <div>
       <MyNav />
         <Switch>
+            <Route path='/'>
+              <SwlLog/>
+            </Route>
             <Route path='/signin'>
               <SignIn />
             </Route>
-            <Route path='/hamform'>
+            <Route>
               <HamLog />
             </Route>
             <Route path='/swllog'>
               <SwlLog />
             </Route>
+            <Route path='/vhflog'>
+              <SwlLog />
+            </Route>
+            <Route path='/contestlog'>
+              <SwlLog />
+            </Route>
         </Switch>
     </div>
+    </Router>
   );
-}
+};
 
 export default App;
